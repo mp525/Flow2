b2aa405f94597fd5664c40c95fcb71596d195a87
matti Hansen <MattiBSH>
2021-03-12 16:37:17 +0100

    2 planes now one random one specific + new staircase to new route

--- Assets/InstantiatePlane.cs
+++ Assets/InstantiatePlane.cs
@@ -26,7 +26,20 @@ public class InstantiatePlane : MonoBehaviour
          {
          Timer = 10f;     
          planeClone = Instantiate(PrefabPlaneWithMotion, new Vector3(487,14,541), transform.rotation) as GameObject;
-           
+        
+
+         float randomx=Random.Range(400,600);
+         if (randomx>483&randomx<490)
+         {
+             randomx=+15;
+         }
+         
+         float randomy=Random.Range(14,30);
+         if (randomy>12&randomy<15)
+         {
+             randomx=+5;
+         }
+           planeClone = Instantiate(PrefabPlaneWithMotion, new Vector3(randomx,randomy,541), transform.rotation) as GameObject;
          }
      }
  }
