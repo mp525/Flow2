a11395ecdeec8275eebe476bc00a3a8f7e657956
matti Hansen <MattiBSH>
2021-03-12 22:06:49 +0100

    2 planes new script adjustment + hangar

--- Assets/InstantiatePlane.cs
+++ Assets/InstantiatePlane.cs
@@ -2,45 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InstantiatePlane : MonoBehaviour
-{   
-      float distance =0;
-      float Timer = 10;
-     public GameObject PrefabPlaneWithMotion;
-     public GameObject player;
+public class InstantiatePlane: MonoBehaviour {
+    float distance = 0;
+    float Timer = 1;
+    public GameObject PrefabPlaneWithMotion;
+    public GameObject player;
 
-     GameObject planeClone;
+    GameObject planeClone;
     // Update is called once per frame
     private void Start() {
-        player=GameObject.FindWithTag("Player");
+        player = GameObject.FindWithTag("Player");
     }
-     void Update()
- {
-     Timer -= Time.deltaTime;
+    void Update() {
+        Timer -= Time.deltaTime;
 
-     if (Timer <= 0f)
-     {
-          distance=541-player.transform.position.z;
-          Debug.Log(distance);
-         if (distance>80)
-         {
-         Timer = 10f;     
-         planeClone = Instantiate(PrefabPlaneWithMotion, new Vector3(487,14,541), transform.rotation) as GameObject;
-        
+        if (Timer <= 0f) {
+            float r = Random.Range(0, 3);
+            
+            if (r == 1) {
+                distance = 541 - player.transform.position.z;
+                Debug.Log(distance);
+                if (distance > 80) {
+                    Timer = 4f;
+                    planeClone = Instantiate(PrefabPlaneWithMotion, new Vector3(487, 14, 541), transform.rotation) as GameObject;
+                }
+                }
+               if (r!=1)
+               {
+                    Timer = 4f;
+                    float randomx = Random.Range(400, 600);
+                    if (randomx > 483 & randomx < 490) {
+                        randomx = +15;
+                    }
 
-         float randomx=Random.Range(400,600);
-         if (randomx>483&randomx<490)
-         {
-             randomx=+15;
-         }
-         
-         float randomy=Random.Range(14,30);
-         if (randomy>12&randomy<15)
-         {
-             randomx=+5;
-         }
-           planeClone = Instantiate(PrefabPlaneWithMotion, new Vector3(randomx,randomy,541), transform.rotation) as GameObject;
-         }
-     }
- }
-}
+                    float randomy = Random.Range(14, 30);
+                    if (randomy > 12 & randomy < 15) {
+                        randomx = +5;
+                    }
+                    planeClone = Instantiate(PrefabPlaneWithMotion, new Vector3(randomx, randomy, 541), transform.rotation) as GameObject; 
+               }
+                  
+                
+            }
+        }
+
+
+
+    }
+
--- Assets/PlaneStart.cs
+++ Assets/PlaneStart.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlaneStart : MonoBehaviour
 {
-    public GameObject gameObject;
+    
     // Start is called before the first frame update
     void Start()
     {
