b32da02d4dc3e5decfee45b92ad1cee2b9ab3ac3
matti Hansen <MattiBSH>
2021-03-13 16:33:39 +0100

    HP system UI integration + tags

--- Assets/Respawn.cs
+++ Assets/Respawn.cs
@@ -1,18 +1,59 @@
  using UnityEngine;
  using System.Collections;
  using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
  public class Respawn : MonoBehaviour {
  
      public GameObject spawnPoint;
      public GameObject tp;
      public GameObject tp2;
 
+     private int currentHealth = 100; 
+    private int maxhealth = 100; 
+      [SerializeField] private HealthDisplay healthDisplay;
+
     private void OnCollisionEnter(Collision other) {
          if(other.transform.tag == "Void")
          {
-              this.transform.position = spawnPoint.transform.position;
+              TakeDamage(110);
+              if (healthDisplay.GetCurrentHealth()<0)
+              {
+                    this.transform.position = spawnPoint.transform.position;
+              }
+         if (healthDisplay.GetCurrentHealth()<0.1)
+        {
+            healthDisplay.SetCurrentHealth(100);
+        }
+             
          }
+
+         if(other.transform.tag == "plane")
+         {
+              TakeDamage(80);
+              if (healthDisplay.GetCurrentHealth()<0)
+              {
+                    this.transform.position = spawnPoint.transform.position;
+              }
+         if (healthDisplay.GetCurrentHealth()<0.1)
+        {
+            healthDisplay.SetCurrentHealth(100);
+        }
+             
+         }
+         if(other.transform.tag == "zombie")
+         {
+              TakeDamage(34);
+              if (healthDisplay.GetCurrentHealth()<0)
+              {
+                    this.transform.position = spawnPoint.transform.position;
+              }
+         if (healthDisplay.GetCurrentHealth()<0.1)
+        {
+            healthDisplay.SetCurrentHealth(100);
+        }
+             
+         }
+
 
          if(other.transform.tag == "TP")
          {
@@ -26,6 +67,12 @@
          if (other.gameObject.tag == "Win"){
           SceneManager.LoadScene (1);
          }
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthDisplay.SetCurrentHealth(healthDisplay.GetCurrentHealth() - damage);
+       
     }
      
  }
